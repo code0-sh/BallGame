4a7d96a44f4914359041a21af209c5bf0d017a44
Shuuichi Omura <omura.522@fssoftware.co.jp>
2015-07-24 15:54:48 +0900

    複数のボールを追加

--- BallGame/GameScene.swift
+++ BallGame/GameScene.swift
@@ -15,19 +15,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func initObjects(){
         // ボール
-        let radius:CGFloat = 20
-        let shape = SKShapeNode(circleOfRadius:radius)
-        shape.fillColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
-        //ランダムに配置
-        var randIntX = radius + (CGFloat)(arc4random_uniform((UInt32)(self.frame.width-radius*2)))
-        var randIntY = radius + (CGFloat)(arc4random_uniform((UInt32)(self.frame.height-radius*2)))
-        shape.position = CGPoint(x:randIntX, y:randIntY)
-        self.addChild(shape)
-        // 重力
-        shape.physicsBody = SKPhysicsBody(circleOfRadius: radius)
-        shape.physicsBody?.restitution = 1.0 // 反発係数
-        shape.physicsBody?.linearDamping = 0.0 // 空気抵抗
-        shape.physicsBody?.mass = 1.0 // 質量
+        for i in 0..<10 {
+            var radius: CGFloat = 20
+            var ball = SKShapeNode(circleOfRadius:radius)
+            ball.fillColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+            // ランダムに配置
+            var randIntX = radius + (CGFloat)(arc4random_uniform((UInt32)(self.frame.width-radius*2)))
+            var randIntY = radius + (CGFloat)(arc4random_uniform((UInt32)(self.frame.height-radius*2)))
+            ball.position = CGPoint(x:randIntX, y:randIntY)
+            self.addChild(ball)
+            // 重力
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: radius)
+            ball.physicsBody?.restitution = 1.0 // 反発係数
+            ball.physicsBody?.linearDamping = 0.0 // 空気抵抗
+            ball.physicsBody?.mass = 1.0 // 質量
+            ball.physicsBody?.friction = 0.0 // 摩擦
+        }
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
@@ -40,6 +43,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
         self.physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        self.physicsBody?.restitution = 1.0 // 反発係数
+        self.physicsBody?.linearDamping = 0.0 // 空気抵抗
+        self.physicsBody?.friction = 0.0 // 摩擦
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
